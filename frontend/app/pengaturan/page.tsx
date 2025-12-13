95ec7d8fd96f276f68d588e48f38385d147de780
loishaard21 <loishaadelliarahmadany@gmail.com>
2025-12-13 12:42:55 +0700

    loisha: mengganti nama website menjadi cerdas finansial di bagian side bar di halaman pengaturan

--- frontend/app/pengaturan/page.tsx
+++ frontend/app/pengaturan/page.tsx
@@ -38,7 +38,7 @@ export default function PengaturanPage() {
 
       {/* SIDEBAR */}
       <aside className="w-60 bg-gray-900 text-white flex flex-col p-6 space-y-6">
-        <h1 className="text-2xl font-bold tracking-wide mb-4">KeuanganKu</h1>
+        <h1 className="text-2xl font-bold tracking-wide mb-4">CERDAS FINANSIAL</h1>
 
         <nav className="flex flex-col space-y-4 text-gray-300">
           <a href="/dashboard" className="hover:text-white">
