ffc96e86ce953b31427bbcf4771896703dcc3680
kevinrizkiantoni <antonikevinrizki@gmail.com>
2025-12-20 11:35:39 +0700

    kevin: menambahkan warna pada background register

--- frontend/app/registrasi/page.tsx
+++ frontend/app/registrasi/page.tsx
@@ -52,7 +52,7 @@ export default function RegisterPage() {
   };
 
   return (
-    <div className="min-h-screen flex justify-center items-center bg-gray-100">
+    <div className="min-h-screen flex justify-center items-center bg-gradient-to-br from-[#FDF8F9] via-white to-[#EED4DB]">
       <div className="bg-white p-8 rounded-xl shadow-xl w-96">
         <h1 className="text-3xl font-bold text-center mb-6">Register</h1>
 
