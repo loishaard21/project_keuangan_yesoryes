2f9c4b0cc1dd77cfa69f95ac1862ec8253292716
kevinrizkiantoni <antonikevinrizki@gmail.com>
2025-12-22 15:16:56 +0700

    kevin: mengganti kolom menjadi warna pink

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -59,13 +59,17 @@ export default function LoginPage() {
         </h1>
 
         <form onSubmit={handleLogin} className="space-y-5">
+          <div>
           <label className="block mb-2 text-sm font-bold text-[#426E55]">Email</label>
           <input
-            type="email"
-            value={form.email}
-            onChange={(e) => handleChange("email", e.target.value)}
-            className="w-full border p-2 rounded mb-4"
-          />
+              type="email"
+              value={form.email}
+              onChange={(e) => handleChange("email", e.target.value)}
+              // Border default "Sleeping pink", Focus "Growing pink", Text "Sleeping green"
+              className="w-full border-2 border-[#D698AB] px-4 py-3 rounded-xl outline-none focus:border-[#CB748E] focus:ring-4 focus:ring-[#CB748E]/20 transition-all text-[#2D4839] placeholder-[#D698AB]/70"
+              placeholder="nama@email.com"
+            />
+          </div>
 
           <label className="block mb-2 font-semibold">Password</label>
           <input
